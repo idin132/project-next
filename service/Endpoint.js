d93bb9307196be52514c5b76ae92577cd8076606
unknown <idinnaufal131204@gmail.com>
2022-11-24 15:29:22 +0700

    first commit

---
+++ service/Endpoint.js
@@ -0,0 +1,13 @@
+// --- API ENDPOINT --- //
+export const BASE_URL="http://localhost:8000"
+
+// --- API LOGIN --- //
+export const LOGIN = BASE_URL + "/api/login"
+export const REGISTER = BASE_URL + "/api/register"
+export const LOGOUUT = BASE_URL + "/api/logout"
+
+// --- API KATEGORI ---//
+export const KATEGORI= BASE_URL + "/api/kategori"
+
+// --- API USER --- //
+export const GET_USER = BASE_URL + "/api/users"
